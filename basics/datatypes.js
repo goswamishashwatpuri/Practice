d826875f052600eaef77e45f7e55491488fbd987
Shashwat Puri <53654934+goswamishashwatpuri@users.noreply.github.com>
2024-04-09 03:15:09 +0000

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,7 +1,7 @@
 // 2 types of datatypes
 // Primitive and Non primitive (also called reference)
 
-// Primitive -> number, bignum, boolean, string, undefined, null, symbol
+// Primitive -> number, bigInt, boolean, string, undefined, null, symbol
 // non proimitive -> Object, Function, Array
 
 // numbers
@@ -11,4 +11,40 @@ let num3 = 1.00000001
 
 // floating point arthematic is not 100% accurate in JS
 console.log(num2+num3);
+
+// bigInt -> put n after a number
+let bInt = 1235402321n
+
+// Cant perform arthmatic of BigInt with a Number
+// console.log(bInt*num1);
+
+let x = null;
+// type of null is an object
+console.log(typeof x);
+
+// symbols
+let sym1 = Symbol(num1)
+console.log(sym1);
+
+const sym2 = Symbol(num1)
+console.log(sym2);
+
+// even though the O/P value of symbol is same, tthey're NOT EQUAL
+// each symbol is unique
+console.log(sym1 === sym2);
+
+// symbols can be reassigned.
+sym1 = 1222
+console.log(sym1);
+
+
+// functions
+let fun = function (inp = 12) {
+    console.log(inp);
+}
+
+// type of this function is - [Function: fun]
+console.log(fun)
+
+fun(1222)
 
