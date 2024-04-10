95464bd446e5635b8a1946986583e9405a5caa7d
Shashwat Puri <53654934+goswamishashwatpuri@users.noreply.github.com>
2024-04-10 11:53:00 +0000

    arrays

--- basics_02/arrays.js
+++ basics_02/arrays.js
@@ -15,3 +15,41 @@ let sampleArr2 = sampleArr1
 sampleArr1[0] = "lorem"
 
 console.log(sampleArr1, sampleArr2)
+
+// creating an array object
+let arrObj = new Array(1,2,3,4,"sample",5,6,7)
+console.log(arrObj);
+
+// push op
+arrObj.push("new element 1")
+
+// pop removes last value in array
+arrObj.pop()
+
+//  Shift - opposite of pop. removes 1st element of the array.
+arrObj.shift()
+console.log(arrObj);
+
+// includes - check if element exists in array - Boolean output
+console.log(arrObj.includes(4))
+
+// indexOf - returns index of an element. If doesn't exist, then returns -1
+console.log(arrObj.indexOf("sample"))
+
+
+// this converts an array to a comma separated element based string
+let sampleJoin = arrObj.join()
+
+// IMPORTANT: typeof sampleJoin -> STRING
+console.log(typeof sampleJoin);
+
+let tempArr = [1,2,3,4,5,6,7,8,9,10]
+
+// slice. range type -> [) . Meaning 1st (start) element index included. 2nd (end) element NOT included.
+// not change in ORIGINAL ARRAY.
+let sliceArr = tempArr.slice(1,3)
+
+// splice
+// IMPORTANT: Splice changes ORIGINAL ARRAY as well. Spliced elements are removed from original array.
+let spliceArr = tempArr.splice(1,3)
+console.log(sliceArr, spliceArr, tempArr)
